98d2fb823926c563e456816cc535392dccbc4157
Niq S. <niqban123@gmail.com>
2025-09-16 18:11:30 +0800

    service-worker back-end change & started on front-end

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+import Heading from "./components/header.tsx"
+import Body from "./components/body.tsx"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Cypher</h1>
+    <Heading />
+    <Body />
   )
 }
 
