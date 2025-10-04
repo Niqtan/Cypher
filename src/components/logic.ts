465d5e6387432d8b0608826c2aa50fc01de9cd03
Niq S. <niqban123@gmail.com>
2025-10-04 08:06:29 +0800

    added api link

--- src/components/logic.ts
+++ src/components/logic.ts
@@ -15,7 +15,7 @@ export async function Summarize(): Promise<SummaryData> {
 
         const pageText = await getPageText(tab.id)
 
-        const response  = await fetch("https://cypher-1amv20ht3-niqs-projects-c135a7ed.vercel.app/api/summarize", {
+        const response  = await fetch("https://cypher-o4lxtqfqc-niqs-projects-c135a7ed.vercel.app/api/summarize", {
             method: "POST",
             headers: {
                 "Authorization": `Bearer ${process.env.GROQ_API_KEY}`,
